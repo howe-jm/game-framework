cbb8f448f3e0b089961c1368e8ece41433790c85
Jason Howe <trisynth@gmail.com>
2021-04-11 12:18:27 -0400

    Added some test data to experiment with changing one part of the state while leaving other parts alone via spread syntax.

--- src/CountDisplay/CountDisplay.js
+++ src/CountDisplay/CountDisplay.js
@@ -2,11 +2,17 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 const CountDisplay = (props) => {
-  return <p>Test: {props.count}</p>;
+  return (
+    <div>
+      <p>Count: {props.count}</p>
+      <p>Test: {props.testCount}</p>
+      {console.log(props)}
+    </div>
+  );
 };
 
 const mapStateToProps = (state) => {
-  return { count: state.count.count };
+  return { count: state.count.count, testCount: state.count.testCount };
 };
 
 export default connect(mapStateToProps)(CountDisplay);
--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -2,19 +2,20 @@ import { actionTypes } from '../actions';
 
 const initialState = {
   count: 0,
+  testCount: 99,
 };
 
 const counterReducer = (state = initialState, action) => {
   const { type, payload } = action;
   switch (type) {
     case actionTypes.INCREMENT_COUNTER:
-      return { count: state.count + 1 };
+      return { ...state, count: state.count + 1 };
     case actionTypes.DECREMENT_COUNTER:
-      return { count: state.count - 1 };
+      return { ...state, count: state.count - 1 };
     case actionTypes.RESET_COUNTER:
-      return { count: 0 };
+      return { ...state, count: 0 };
     case actionTypes.CHANGE_COUNT:
-      return { count: payload };
+      return { ...state, count: payload };
     default:
       return state;
   }
